50adc710be8a666c503e265095d8d3a6ae14d986
yanmin <shaoyan.alpha@gmail.com>
2014-02-18 21:52:04 +0800

    Declare a model

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,5 +1,30 @@
 require "my_mongoid/version"
 
 module MyMongoid
-  # Your code goes here...
+  # This module defines all the configuration options for Mongoid
+  module Config
+    def models
+      @models ||= []
+    end
+
+    def register_model(klass)
+      @models.push(klass) unless models.include?(klass)
+    end
+  end
+
+  extend MyMongoid::Config
+
+  # This is the base module for all domain objects
+  module Document
+    module ClassMethods
+      def is_mongoid_model?
+        true
+      end
+    end
+
+    def self.included(klass)
+      klass.extend(ClassMethods)
+      MyMongoid.register_model(klass)
+    end
+  end
 end
