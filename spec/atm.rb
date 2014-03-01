d906a6254300b613d336e4c5c53020478becc7aa
Allen Wu <allenwu1018@gmail.com>
2014-03-01 16:47:38 +0800

    temp commit: practicing ActiveSupport::Callbacks

--- spec/atm.rb
+++ spec/atm.rb
@@ -1,9 +1,59 @@
 require "active_support"
 
+module Callbacks
+  extend ActiveSupport::Concern
+
+  included do
+    define_atm_callbacks :deposit, :withdraw
+  end
+
+  module ClassMethods
+    def define_atm_callbacks(*callbacks)
+      options = { :terminator => "result == false" }
+      callbacks.each do |callback|
+        define_callbacks callback, options
+      end
+
+      types = [:before, :after, :around]
+      types.each do |type|
+        send("_define_command_#{type}_callback", self)
+      end
+    end
+
+    private
+
+    def _define_command_before_callback(klass)
+      klass.class_eval <<-CALLBACK, __FILE__, __LINE__ + 1
+        def self.command_before(callback, *args, &block)
+          set_callback(callback, :before, *args, &block)
+        end
+      CALLBACK
+    end
+
+    def _define_command_around_callback(klass)
+      klass.class_eval <<-CALLBACK, __FILE__, __LINE__ + 1
+        def self.command_around(callback, *args, &block)
+          set_callback(callback, :around, *args, &block)
+        end
+      CALLBACK
+    end
+
+    def _define_command_after_callback(klass)
+      klass.class_eval <<-CALLBACK, __FILE__, __LINE__ + 1
+        def self.command_after(callback, *args, &block)
+          set_callback(callback, :after, *args, &block)
+        end
+      CALLBACK
+    end
+
+  end
+end
+
 class Account
-  attr_reader :balance
-  def initialize(balance)
+  attr_reader :balance, :valid
+  def initialize(balance, valid = true)
     @balance = balance
+    @valid = true
   end
 
   def deposit(amount)
@@ -15,12 +65,21 @@ class Account
   end
 
   def valid_access?
-    true
+    @valid
   end
 end
 
+
+
 class ATM
+  include ActiveSupport::Callbacks
+  include Callbacks
+
   attr_reader :account
+
+  define_callbacks :deposit
+  command_around :deposit, ->(r, &block) { log("before log"); block.call; log("after log") }
+
   def initialize(account)
     @account = account
   end
@@ -33,7 +92,9 @@ module ATM::Commands
   end
 
   def deposit(amount)
-    account.deposit(amount)
+    run_callbacks(:deposit) do
+      account.deposit(amount)
+    end
     amount
   end
 end
@@ -58,21 +119,26 @@ module ATM::SMSNotification
   extend ActiveSupport::Concern
 
   def send_sms(msg)
-    # fake send
+    puts "send FUCK"
   end
 end
+
 
 module ATM::Concerns
   extend ActiveSupport::Concern
 
   included do
-    include ActiveSupport::Callbacks
     include ATM::Commands
     include ATM::Authentication
     include ATM::Logging
     include ATM::SMSNotification
+    include ActiveSupport::Callbacks
   end
 end
 
-#ATM.send :include, ATM::Concerns
-ATM.include ATM::Concerns
+ATM.send :include, ATM::Concerns
+
+#account = Account.new(1000)
+#atm = ATM.new(account)
+
+#atm.deposit(100)
