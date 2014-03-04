53693cb0f85f53eeb890ee5ff0a4e508824463d5
august-zou <zjzouxu@gmail.com>
2014-03-04 20:15:33 +0800

    finish lifecycle day1

--- spec/my_mongoid/lifecycle_spec.rb
+++ spec/my_mongoid/lifecycle_spec.rb
@@ -1,226 +1,112 @@
+require "spec_helper"
 
-class MyEvent
-  include MyMongoid::Document
-
-  field :public
-  field :created_at
-
-  before_save :save_foo
-  
-  before_create :create_foo
-
-
-  def save_foo
+describe "Should define lifecycle callbacks" do
+  def config_db
+    MyMongoid.configure do |config|
+      config.host = "127.0.0.1:27017"
+      config.database = "my_mongoid_test"
+    end
   end
 
-  def create_foo
+  def clean_db
+    klass.collection.drop
   end
 
-  def foo
-  end
-end
-
-def config_db
-  MyMongoid.configure do |config|
-    config.host = "localhost:27017"
-    config.database = "my_mongoid_test"
-  end
-end
-
-def clean_my_event_db
-  MyEvent.collection.drop
-end
-
-describe MyMongoid do
   before {
     config_db
-    clean_my_event_db
+    clean_db
   }
-  describe "all hooks" do
-    it "should declare before hook for delete" do
-      expect {
-        MyEvent.module_eval do
-          before_delete :foo
-        end
-        }.to_not raise_error
-    end
 
+  let(:base) {
+    Class.new {
+      include MyMongoid::Document
 
-    it "should declare around hook for delete" do
-      expect {
-        MyEvent.module_eval do
-          around_delete :foo
-        end
-        }.to_not raise_error
-    end
-
-
-    it "should declare after hook for delete" do
-      expect {
-        MyEvent.module_eval do
-          after_delete :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare before hook for save" do
-      expect {
-        MyEvent.module_eval do
-          before_save :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare around hook for save" do
-      expect {
-        MyEvent.module_eval do
-          around_save :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare after hook for save" do
-      expect {
-        MyEvent.module_eval do
-          after_save :foo
-        end
-        }.to_not raise_error
-    end
-
-
-    it "should declare before hook for create" do
-      expect {
-        MyEvent.module_eval do
-          before_create :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare around hook for create" do
-      expect {
-        MyEvent.module_eval do
-          around_create :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare after hook for create" do
-      expect {
-        MyEvent.module_eval do
-          after_create :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare before hook for update" do
-      expect {
-        MyEvent.module_eval do
-          before_update :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare around hook for update" do
-      expect {
-        MyEvent.module_eval do
-          around_update :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should declare after hook for update" do
-      expect {
-        MyEvent.module_eval do
-          after_update :foo
-        end
-        }.to_not raise_error
-    end
-
-    it "should not declare before hook for find" do
-      expect {
-        MyEvent.module_eval do
-          before_find :foo
-        end
-        }.to raise_error
+      def self.name
+        self.to_s
       end
 
-    it "should not declare around hook for find" do
-      expect {
-        MyEvent.module_eval do
-          around_find :foo
-        end
-        }.to raise_error
+      def self.to_s
+        "Event"
+      end
+    }
+  }
+
+
+  describe "all hooks:" do
+    let(:klass) { base }
+    [:delete,:save,:create,:update].each do |name|
+      it "should declare before hook for #{name}" do
+        expect(klass).to respond_to("before_#{name}")
+      end
+
+      it "should declare around hook for #{name}" do
+        expect(klass).to respond_to("around_#{name}")
+      end
+
+      it "should declare after hook for #{name}" do
+        expect(klass).to respond_to("after_#{name}")
+      end
     end
+  end
 
-    it "should declare after hook for find" do
-      expect {
-        MyEvent.module_eval do
-          after_find :foo
-        end
-        }.to_not raise_error
+  describe "only before hooks:" do
+    let(:klass) { base }
+    [:find,:initialize].each do |name|
+      it "should not declare before hook for #{name}" do
+        expect(klass).to_not respond_to("before_#{name}")
+      end
+
+      it "should not declare around hook for #{name}" do
+        expect(klass).to_not respond_to("around_#{name}")
+      end
+
+      it "should declare after hook for #{name}" do
+        expect(klass).to respond_to("after_#{name}")
+      end
     end
-
-    it "should not declare before hook for initialize" do
-      expect {
-        MyEvent.module_eval do
-          before_initialize :foo
-        end
-        }.to raise_error
-    end
-
-    it "should not declare around hook for initialize" do
-      expect {
-        MyEvent.module_eval do
-          around_initialize :foo
-        end
-        }.to raise_error
-    end
-
-
-    it "should declare after hook for initialize" do
-      expect {
-        MyEvent.module_eval do
-          after_initialize :foo
-        end
-        }.to_not raise_error
-    end                                     
   end
 
   describe "run create callbacks" do
-    let(:attributes) {
-      {"public" => true, "created_at" => Time.parse("2014-02-13T03:20:37Z")}
+    let(:klass) {
+      Class.new(base) {
+        before_create :before_method
+      }
     }
 
-    let(:my_event){ MyEvent.new(attributes) }
-    
+    let(:record) {
+      klass.new({})
+    }
+
     it "should run callbacks when saving a new record" do
-      expect(my_event).to receive(:create_foo)
-      my_event.save
+      expect(record).to receive(:before_method)
+      record.save
     end
 
     it "should run callbacks when creating a new record" do
-      expect_any_instance_of(MyEvent).to receive(:create_foo)
-      MyEvent.create(attributes)
+      expect_any_instance_of(klass).to receive(:before_method)
+      klass.create({})
     end
   end
 
   describe "run save callbacks" do
-    let(:attributes) {
-      {"public" => true, "created_at" => Time.parse("2014-02-13T03:20:37Z")}
+    let(:klass) {
+      Class.new(base) {
+        before_save :before_method
+        def before_method
+        end
+      }
     }
 
-    let(:my_event){ MyEvent.new(attributes) }
-
     it "should run callbacks when saving a new record" do
-      my_event
-      expect(my_event).to receive(:save_foo)
-      my_event.save
+      record = klass.new({})
+      expect(record).to receive(:before_method)
+      record.save
     end
 
     it "should run callbacks when saving a persisted record" do
-      my_event = MyEvent.create(attributes)
-      expect(my_event).to receive(:save_foo)
-      my_event.save
+      record = klass.create({})
+      expect(record).to receive(:before_method)
+      record.save
     end
 
   end
